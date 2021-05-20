397109ed6aa56727dc28dffa92bba530f3bafda6
Patrick Luber <patrickluber7@gmail.com>
2021-05-20 13:43:53 +0200

    Kommentare eingefügt

--- Programme/Wlan/Wlan.ino
+++ Programme/Wlan/Wlan.ino
@@ -3,20 +3,23 @@
 const char* ssid = "";
 const char* password = "";
 void setup() {
+  //Initalisieren des Seriellen Monitors
   Serial.begin(115200);
   delay(800);
+  //Starten des WiFi Client
   WiFi.begin(ssid, password);
   delay(500);
   Serial.println();
   int counter = 0;
   Serial.print("Connecting to WiFi");
   while ((WiFi.status() != WL_CONNECTED)) {
+    // Auf verbindung warten
     counter++;
     delay(1000);
     if(counter % 20 == 0){
-      Serial.println(".");
+      Serial.println(".");//Zeilenumbruch alle 20 Zeichen
     }else if(counter % 50 == 0){
-      WiFi.begin(ssid, password);
+      WiFi.begin(ssid, password); //Retry alle 50 Versuche
       Serial.println("Retry");
     }else
     {
@@ -27,7 +30,6 @@ void setup() {
   Serial.println("Connected");
   
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
 
